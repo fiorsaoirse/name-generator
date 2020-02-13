2b42039da969204f53c17ce738e61691a4d07035
fiorsaoirse <solovyanovamaria@yandex.ru>
2020-02-14 00:23:09 +0300

    Changed app start logic

--- src/bin/www.js
+++ src/bin/www.js
@@ -6,8 +6,6 @@ import app from '../app';
 const debug = debugFunc('name-gen-server');
 const port = process.env.PORT || 3000;
 
-app.set('port', port);
-
-const server = app.listen(app.get('port'), () => {
+const server = app.listen(port, () => {
   debug(`Express server listening on port ${server.address().port}`);
 });
