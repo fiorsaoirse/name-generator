51b44630e0fa813cc4aa572185531d2f4e173716
fiorsaoirse <solovyanovamaria@yandex.ru>
2020-02-14 17:50:57 +0300

    Removed debug from starting server

--- src/bin/www.js
+++ src/bin/www.js
@@ -3,9 +3,8 @@
 import debugFunc from 'debug';
 import app from '../app';
 
-const debug = debugFunc('name-gen-server');
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
-  debug(`Express server listening on port ${server.address().port}`);
+  console.log(`Express server listening on port ${port}`);
 });
